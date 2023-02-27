c49fe548be573636ca3f9d915ca813243a9cbf7b
gio <gabusha777@gmail.com>
2023-02-27 22:46:26 +0400

    work

---
+++ WORK1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WORK1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("giorgi gabodze");
+            Console.WriteLine("your name and last name ");
+            Console.WriteLine(Console.ReadLine());
+           
+          
+               
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
